5a1a721e202a5f04f72a8adffeabdcda649f2740
crxssf <crsgrrl@gmail.com>
2021-12-29 16:08:40 -0600

    2ND commit

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -19,4 +19,33 @@ class SiteController extends AbstractController
     {
         return $this->render('site/index.html.twig');
     }
+    public function contacto(): Response
+    {
+        return $this->render('site/contactanos.html.twig');
+    }
+    public function colaborador(): Response
+    {
+        return $this->render('site/colaboradores.html.twig');
+    }
+    public function conocenos(): Response
+    {
+        return $this->render('site/conocenos.html.twig');
+    }
+    public function servicio(): Response
+    {
+        return $this->render('site/servicios.html.twig');
+    }
+    public function producto(): Response
+    {
+        return $this->render('site/productos.html.twig');
+    }
+    public function pro_ensayo(): Response
+    {
+        return $this->render('site/productos-ensayo.html.twig');
+    }
+    public function pro_geologia(): Response
+    {
+        return $this->render('site/productos-geologia.html.twig');
+    }
+
 }
