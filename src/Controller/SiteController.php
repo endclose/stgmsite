a2ccea11b2ecf50cace4d8e5d237c825a6dc1f45
crxssf <crsgrrl@gmail.com>
2022-01-07 15:41:34 -0600

    4TH commit

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -8,68 +8,140 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SiteController extends AbstractController
 {
     /**
-     * @Route("/{_locale}/",
+     * @Route("/{_locale}/index",
      *         name="index",
      *         requirements ={
      *              "_locale": "en|pt|es",
      *         }
      * )
+     * 
      */
+    
     public function index(): Response
     {
         return $this->render('site/index.html.twig');
     }
+
+    /**
+     * @Route("/{_locale}/products{category}",
+     *         name="products",
+     * 
+     *         requirements ={
+     *              "_locale": "en|pt|es",
+     *         }
+     * )
+     * 
+     */
+
+    public function producs ($category): Response
+    {
+        $template = "site/products.html.twig";
+
+        switch ($category)
+        {
+            case "geology":
+                $template = "site/products/geology.html.twig";
+            break;
+
+            case "metarllurgical":
+                $template = "site/products/metallurgical.html.twig";
+            break;
+
+            case "chemical":
+                $template = "site/products/chemical-reagents.html.twig";
+            break;
+
+            case "assay":
+                $template = "site/products/fire-assay.html.twig";
+            break;
+
+            case "equipment":
+                $template = "site/products/lab-equipment.html.twig";
+            break;
+
+            case "sample":
+                $template ="site/products/sample-prep.html.twig";
+            break;
+
+            case "reference":
+                $template = "site/products/reference-materials.html.twig";
+            break;
+
+            case "instruments":
+                $template = "site/products/instruments.html.twig";
+            break;
+        }
+
+        return $this->render($template);
+
+    }
+
     public function contact(): Response
     {
         return $this->render('site/contact-us.html.twig');
     }
+
+    /**
+     * @Route("/{_locale}/partners",
+     *         name="partners",
+     * 
+     *         requirements ={
+     *              "_locale": "en|pt|es",
+     *         }
+     * )
+     * 
+     */
     public function partners(): Response
     {
         return $this->render('site/partners.html.twig');
     }
+
+    /**
+     * @Route("/{_locale}/about-us",
+     *         name="about-us",
+     * 
+     *         requirements ={
+     *              "_locale": "en|pt|es",
+     *         }
+     * )
+     * 
+     */
+
     public function about(): Response
     {
         return $this->render('site/about-us.html.twig');
     }
+
+    /**
+     * @Route("/{_locale}/services",
+     *         name="services",
+     * 
+     *         requirements ={
+     *              "_locale": "en|pt|es",
+     *         }
+     * )
+     * 
+     */
+
     public function services(): Response
     {
         return $this->render('site/services.html.twig');
     }
+
+    /**
+     * @Route("/{_locale}/products",
+     *         name="products",
+     * 
+     *         requirements ={
+     *              "_locale": "en|pt|es",
+     *         }
+     * )
+     * 
+     */
+
     public function products(): Response
     {
         return $this->render('site/products.html.twig');
-    }
-    public function sample(): Response
-    {
-        return $this->render('site/products/sample-prep.html.twig');
-    }
-    public function geology(): Response
-    {
-        return $this->render('site/products/geology.html.twig');
-    }
-    public function assay(): Response
-    {
-        return $this->render('site/products/fire-assay.html.twig');
-    }
-    public function instruments(): Response
-    {
-        return $this->render('site/products/instruments.html.twig');
-    }
-    public function chemical (): Response
-    {
-        return $this->render('site/products/chemical-reagents.html.twig');
-    }
-    public function equipment (): Response
-    {
-        return $this->render('site/products/lab-equipment.html.twig');
-    }
-    public function metallurgical (): Response
-    {
-        return $this->render('site/products/metallurgical.html.twig');
-    }
-    public function reference (): Response
-    {
-        return $this->render('site/products/reference-materials.html-twig'); 
     }
 
 
